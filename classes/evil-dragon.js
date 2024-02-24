87d0c9d05c7a9a3aa88767e71d3989202e3bc97e
Liam Dinh <loiphatdinh@gmail.com>
2024-02-23 19:22:13 -0600

    finished Phase 3 - Creating Evil Dragon and its methods

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,28 @@
 // Your code here
+
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    // dontInviteThemOverForDinner() -- this method should console.log out each of the Evil
+    // Dragon's evil doings separately, in the format ${name} wil ${evilDoing}
+    dontInviteThemOverForDinner() {
+        this.evilDoings.forEach(evilDoing => {
+            console.log(`${this.name} will ${evilDoing}`);
+        })
+    }
+
+    // burnsNemesis() - this method should return a string using name and nemesis property values formatted as ${name}
+    // destroys ${nemesis} with fire! WHOOOSH!!!
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+
+}
+
+module.exports = EvilDragon;
